df5cc8f1fcc6cb265f2c66bb8e77ab1daa6fa627
JackieZheng0203 <p17215344@my365.dmu.ac.uk>
2020-03-11 22:49:21 +0000

    Apply script function for navigation bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+"use strict";
+
 console.log("hello");
+
+const navbarToggle = document.getElementById("navbar-toggle");
+const menuNav = document.getElementById("menu-nav");
+
+const toggleMenu = () => {
+  menuNav.classList.toggle("navbar-toggle");
+}
+
+navbarToggle.addEventListener("click", toggleMenu);
