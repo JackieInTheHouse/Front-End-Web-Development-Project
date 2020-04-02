16f57ca40f17782ae78adc1d4da35ac3b434054f
JackieZheng0203 <p17215344@my365.dmu.ac.uk>
2020-04-02 11:19:34 +0100

    Decoupled the css from javascript done

--- js/scripts.js
+++ js/scripts.js
@@ -36,15 +36,12 @@ function showImages(n) {
   if (n < 1) {
     imageIndex = images.length}
   for (i = 0; i < images.length; i++) {
-      // images[i].style.display = "none";
-        images[i].classList.remove("active");
+    images[i].classList.remove("active");
   }
 
   for (i = 0; i < dots.length; i++) {
     dots[i].classList.remove("active");
-      // dots[i].className = dots[i].className.replace(" active", "");
   }
-  // images[imageIndex-1].style.display = "block";
   images[imageIndex-1].classList.add("active");
   dots[imageIndex-1].classList.add("active");
 }
